69f51cc16bd1e9c2c83ff18f7485935083b6a2f3
Urban Etter <uetter@gmail.com>
2017-12-09 20:58:47 +0100

    Implemented play and setStyle

--- src/Controller/JassWeCanController.php
+++ src/Controller/JassWeCanController.php
@@ -4,22 +4,35 @@ namespace App\Controller;
 
 use App\Repository;
 use function Jass\CardSet\jassSet;
+use Jass\Entity\Card;
 use Jass\Message\Deal;
 use Jass\Message\PlayerSetup;
+use Jass\Message\StyleSetup;
+use Jass\Message\Turn;
+use Jass\MessageHandler;
 use function Jass\Player\byNames;
 use Jass\Strategy\Bock;
+use function Jass\Strategy\cardStrategy;
 use Jass\Strategy\Simple;
 use Jass\Strategy\TeamMate;
 use Jass\Strategy\Trump;
+use Jass\Style\BottomUp;
+use Jass\Style\TopDown;
+use Jass\Style\Trump as TrumpStyle;
+use function Jass\Trick\playedCards;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * @Route("/jwc")
+ */
 class JassWeCanController extends Controller
 {
     /**
-     * @Route("/jwc", name="create")
+     * @Route("/", name="create")
      * @Method({"POST"})
      * @param Repository $repository
      * @return JsonResponse
@@ -59,5 +72,103 @@ class JassWeCanController extends Controller
         ];
 
         return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/{id}", name="info")
+     * @Method({"GET"})
+     * @param string $id
+     * @param Repository $repository
+     * @return JsonResponse
+     */
+    public function info(string $id, Repository $repository)
+    {
+        $game = $repository->loadGame($id);
+
+        $data = [
+            'id' => $game->name,
+            'hand' => $game->players[0]->hand
+        ];
+
+        if ($game->style) {
+            $data['style'] = $game->style->name;
+        }
+
+        if ($game->currentTrick) {
+            $data['trick'] = playedCards($game->currentTrick);
+        }
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/{id}", name="play")
+     * @Method({"POST"})
+     * @param string $id
+     * @param Request $request
+     * @param Repository $repository
+     * @return JsonResponse
+     */
+    public function play(string $id, Request $request, Repository $repository)
+    {
+        $input = json_decode($request->getContent(), true);
+
+        $card = Card::from($input['card']['suit'], $input['card']['value']);
+
+        $playCard = new Turn();
+        $playCard->card = $card;
+
+        $repository->recordMessage($id, $playCard);
+        $game = $repository->loadGame($id);
+
+
+        $messageHandler = new MessageHandler();
+        $playedCards = [];
+        do {
+            $turn = new Turn();
+            $turn->card = \Jass\Strategy\card($game);
+            $messageHandler->handle($game, $turn);
+            $repository->recordMessage($id, $turn);
+            $playedCards[] = $turn->card;
+        } while ($game->currentPlayer !== $game->players[0]);
+
+        $strategy = cardStrategy($game);
+
+        $data = [
+            'playedCards' => $playedCards,
+            'hint' => $strategy->chooseCard($game),
+            'hintReason' => get_class($strategy),
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/{id}/style", name="style")
+     * @Method({"POST"})
+     * @param string $id
+     * @param Request $request
+     * @param Repository $repository
+     * @return JsonResponse
+     */
+    public function style(string $id, Request $request, Repository $repository)
+    {
+        $input = json_decode($request->getContent(), true);
+
+        list($first, $second) = explode(' ', $input['style']);
+        if ($first === 'trump') {
+            $style = new TrumpStyle($second);
+        } elseif ($first === 'top') {
+            $style = new TopDown();
+        } else {
+            $style = new BottomUp();
+        }
+
+        $styleSetup = new StyleSetup();
+        $styleSetup->style = $style;
+
+        $repository->recordMessage($id, $styleSetup);
+
+        return $this->info($id, $repository);
     }
 }
